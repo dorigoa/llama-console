93c1c1ce16a7fba0265470fccd0b2bd3818b0053
dorigo_a <alvise.dorigo@psi.ch>
2026-05-03 11:04:41 +0200

    command to add conf tailscale to access gui/openwebui from remote via ssl

---
+++ scripts/Tailscale/create_ssl_endpoints.sh
@@ -0,0 +1,4 @@
+/Applications/Tailscale.app/Contents/MacOS/Tailscale serve reset
+/Applications/Tailscale.app/Contents/MacOS/Tailscale serve --bg --https=8443 http://127.0.0.1:8080
+/Applications/Tailscale.app/Contents/MacOS/Tailscale serve --bg --https=443  http://127.0.0.1:8000
+/Applications/Tailscale.app/Contents/MacOS/Tailscale serve --bg --https=9443 http://127.0.0.1:8088
