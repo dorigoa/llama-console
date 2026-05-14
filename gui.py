482067560d48a868f038dfafe13221eee5de4c77
dorigo_a <alvise.dorigo@psi.ch>
2026-05-14 21:41:22 +0200

    fix

--- gui.py
+++ gui.py
@@ -781,7 +781,7 @@ with ui.column().classes("w-full max-w-4xl mx-auto p-4 gap-4"):
     with ui.row().classes("w-full gap-4 mt-4 items-end"):
         ui.label("Server Logs").classes("flex-1")
         clear_log = ui.button("Clear Logs", on_click=clear_log, icon="delete").classes("mt-4")
-    log_area = ui.log().classes("w-full h-96 font-mono text-xs bg-black text-green-400 ")
+    #log_area = ui.log().classes("w-full h-96 font-mono text-xs bg-black text-green-400 ")
     log_area = (
         ui.log()
         .classes("w-full h-96 font-mono text-xs bg-black text-green-400")
