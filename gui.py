c4222e907927d88a6694199e9e433138f64cf73c
dorigoa <alvise72@live.com>
2026-05-18 13:34:26 +0200

    Update gui.py
    Bold font for all cards' titles

--- gui.py
+++ gui.py
@@ -731,7 +731,7 @@ with ui.column().classes("w-full max-w-4xl mx-auto p-4 gap-4"):
     ####################################
 
     with ui.card().classes("w-full p-4"):
-        ui.label("Select a model").classes("text-subtitle1")
+        ui.label("Select a model").classes("text-subtitle1 font-bold")
 
         with ui.row().classes("w-full gap-4 mt-4 items-end"):
 
@@ -867,7 +867,7 @@ with ui.column().classes("w-full max-w-4xl mx-auto p-4 gap-4"):
         log_area.clear()
 
     with ui.row().classes("w-full gap-4 mt-4 items-end"):
-        ui.label("Server Logs").classes("flex-1")
+        ui.label("Server Logs").classes("flex-1 fond-bold")
         clear_log = ui.button("Clear Logs", on_click=clear_log, icon="delete").classes("mt-4")
     
     log_area = (
