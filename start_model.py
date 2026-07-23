47ff8905932054cc24700fb14a02f6f69a7d5faf
dorigo_a <alvise.dorigo@psi.ch>
2026-07-23 23:12:38 +0200

    removed old code

--- start_model.py
+++ start_model.py
@@ -56,60 +56,6 @@ def _get_first_model_name(endpoint: str) -> tuple[str,int] | None:
     except json.JSONDecodeError as e:
         raise RuntimeError(f"JSON parsing error: {e}") from e
 
-# #___________________________________________________________________________________
-# def _build_command(binary: str, model: Model, devices: str = "", ctx: int | None = None) -> list[str]:
-#     cmd = [binary, "-m", str(model.model_path), "-c", str(ctx if ctx is not None else model.ctxsize)]
-
-#     if model.fitt:
-#         cmd += ["-fitt", model.fitt]
-
-#     if model.rpcservers:
-#         rpc_list = ",".join(f"{s.IP}:{s.PORT}" for s in model.rpcservers)
-#         cmd += ["--rpc", rpc_list]
-
-#     if devices:
-#         cmd += ["--device", devices]
-
-#     if model.mmproj_path and str(model.mmproj_path).lower() not in ("none", "null", ""):
-#         cmd += ["--mmproj", str(model.mmproj_path)]
-
-#     data = {"reasoning_effort": model.reasoning}
-
-#     #cmd += ["--chat-template", "chatml"]
-#     cmd += ["--host", settings.ADDRESS_BIND]
-#     cmd += ["--port", str(settings.PORT_BIND)]
-#     cmd += ["--split-mode", "layer"]
-#     cmd += ["--metrics"]
-#     cmd += ["--jinja"]
-#     cmd += ["-fa", "on"]
-#     cmd += ["-fit", "on"] # Using "on" makes the rpc/Vulkan on PC with 2 NVidia cards crash
-#     cmd += ["-fitc", "8192"]
-#     cmd += ["-np", "1"] # Required by MTP processing
-#     cmd += ["--no-warmup"]
-#     cmd += ["--temp", str(model.temperature)]
-#     cmd += ["--top-p", str(model.top_p)]
-#     cmd += ["--top-k", str(model.top_k)]
-#     cmd += ["--chat-template-kwargs", json.dumps(data)]
-#     cmd += ["--seed", "123456789"]
-#     if model.kvquant:
-#         cmd += ["-ctk", model.kvquant]
-#         cmd += ["-ctv", model.kvquant]
-#     cmd += ["--alias", str(model.alias)]
-#     if model.min_p >= 0:
-#         cmd += ["--min-p", str(model.min_p)]
-#     #if model.extras:
-#     #    cmd += model.extras
-#     if model.ub:
-#         cmd += ["-ub", str(model.ub)]
-#     if model.b:
-#         cmd += ["-b", str(model.b)]
-#     if model.mtp:
-#         cmd += ["--spec-type", "draft-mtp"]
-#     # Log file for detached execution (UI polls it)
-#     cmd += ["--log-file", LLAMA_LOG_FILE]
-
-#     return cmd
-
 #___________________________________________________________________________________
 def _ssh_dest() -> str | None:
     if not settings.LLAMA_SERVER_HOST:
